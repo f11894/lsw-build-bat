7b8810a3b66964f8642cd57c183fb7169f848360
fg118942 <16046279+f11894@users.noreply.github.com>
2020-06-15 20:11:02 +0900

    フォルダ名にpatchのバージョンではなくffmpegのバージョンを使うように変更

--- sh/ffmpeg.sh
+++ sh/ffmpeg.sh
@@ -1,12 +1,14 @@
 FFmpeg_ver=4.2.2
 FFmpeg_patch_ver=4.2.2
 FFmpeg_patch=https://gist.github.com/maki-rxrz/5a7a2c789e4369fa34853b5358fb8a29/raw/8f3016b66a025588b9d361838ed000e8dae30e28/ffmpeg-4.2.2-allpatches.diff
-if [ ! -d ffmpeg-${FFmpeg_patch_ver} ]; then
-    git clone --depth=1 -b n${FFmpeg_ver} git://source.ffmpeg.org/ffmpeg.git ffmpeg-${FFmpeg_patch_ver}
+if [ ! -d ffmpeg-${FFmpeg_ver} ]; then
+    git clone --depth=1 -b n${FFmpeg_ver} git://source.ffmpeg.org/ffmpeg.git ffmpeg-${FFmpeg_ver}
+    pushd ffmpeg-${FFmpeg_ver}
     curl -O -L ${FFmpeg_patch}
-    patch -p0 <ffmpeg-${FFmpeg_patch_ver}-allpatches.diff
+    patch -p1 <ffmpeg-${FFmpeg_patch_ver}-allpatches.diff
+    popd
 fi
-cd ffmpeg-${FFmpeg_patch_ver}
+cd ffmpeg-${FFmpeg_ver}
 ./configure --target-os=mingw32 --prefix=/mingw32/ --disable-shared --disable-debug --disable-encoders --disable-doc $1 --enable-libmfx --enable-static --enable-gpl --enable-version3 --enable-runtime-cpudetect --enable-avisynth
 make clean
 make uninstall
