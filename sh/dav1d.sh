52c71c7cf888f2406193ef7b740d095c8577e3da
fg118942 <16046279+f11894@users.noreply.github.com>
2021-09-04 10:56:06 +0900

    [workaround] ビルドに失敗するのでdav1dの0.9.2を使うように変更

--- sh/dav1d.sh
+++ sh/dav1d.sh
@@ -1,9 +1,11 @@
 if [ ! -d dav1d ]; then
     git clone https://code.videolan.org/videolan/dav1d
 else
-    git -C dav1d pull | grep 'Already up to date.' && exit
+    git -C dav1d checkout -f master
+    git -C dav1d pull
 fi
 cd dav1d
+git checkout -f 0.9.2
 if [  -d dav1d_build ]; then
     rm -rf dav1d_build
 fi
